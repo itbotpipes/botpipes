c09b98c9ca7f958b285c1b502e2fdc79a7a85ed0
PrachiBurate <prachiburate13@gmail.com>
2026-05-27 15:04:37 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typedRoutes: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   images: {
     remotePatterns: [
       {
